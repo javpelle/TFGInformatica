54d14f52c1d07bb7f91817eba20a5e2be63a6c39
Javier Pellejero <javier_pellejer@hotmail.com>
2020-02-12 22:39:25 +0100

    qsharpOperators List finished

--- src/MTQC/src/model/Model.java
+++ src/MTQC/src/model/Model.java
@@ -8,6 +8,33 @@ import model.mutantoperator.qiskit.AndOr;
 import model.mutantoperator.qiskit.OrAnd;
 import model.mutantoperator.qsharp.GateHX;
 import model.mutantoperator.qsharp.GateHY;
+import model.mutantoperator.qsharp.GateHZ;
+import model.mutantoperator.qsharp.GateST;
+import model.mutantoperator.qsharp.GateTS;
+import model.mutantoperator.qsharp.GateXH;
+import model.mutantoperator.qsharp.GateXY;
+import model.mutantoperator.qsharp.GateXZ;
+import model.mutantoperator.qsharp.GateYH;
+import model.mutantoperator.qsharp.GateYX;
+import model.mutantoperator.qsharp.GateYZ;
+import model.mutantoperator.qsharp.GateZH;
+import model.mutantoperator.qsharp.GateZX;
+import model.mutantoperator.qsharp.GateZY;
+import model.mutantoperator.qsharp.MissAdjoint;
+import model.mutantoperator.qsharp.OneZero;
+import model.mutantoperator.qsharp.PauliXY;
+import model.mutantoperator.qsharp.PauliXZ;
+import model.mutantoperator.qsharp.PauliYX;
+import model.mutantoperator.qsharp.PauliYZ;
+import model.mutantoperator.qsharp.PauliZX;
+import model.mutantoperator.qsharp.PauliZY;
+import model.mutantoperator.qsharp.RotXY;
+import model.mutantoperator.qsharp.RotXZ;
+import model.mutantoperator.qsharp.RotYX;
+import model.mutantoperator.qsharp.RotYZ;
+import model.mutantoperator.qsharp.RotZX;
+import model.mutantoperator.qsharp.RotZY;
+import model.mutantoperator.qsharp.ZeroOne;
 
 public class Model implements Observable<Observer> {
 
@@ -22,7 +49,11 @@ public class Model implements Observable<Observer> {
 
 	private MutantOperator[] qiskitOperators = { new AndOr(), new OrAnd() };
 
-	private MutantOperator[] qsharpOperators = { new GateHX(), new GateHY() };
+	private MutantOperator[] qsharpOperators = { new GateHX(), new GateHY(), new GateHZ(), new GateST(), new GateTS(),
+			new GateXH(), new GateXY(), new GateXZ(), new GateYH(), new GateYX(), new GateYZ(), new GateZH(),
+			new GateZX(), new GateZY(), new MissAdjoint(), new OneZero(), new PauliXY(), new PauliXZ(), new PauliYX(),
+			new PauliYZ(), new PauliZX(), new PauliZY(), new RotXY(), new RotXZ(), new RotYX(), new RotYZ(),
+			new RotZX(), new RotZY(), new ZeroOne() };
 
 	public Model() {
 		qiskit = false;
